294ef375e24a54df3a068933fd4ae6e2156acab8
Antonio Marcos Patricio Castro <54753638+TonyMPCastro@users.noreply.github.com>
2026-07-15 19:31:33 -0300

    feat: adiciona middleware para proteger rota admin

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+  function middleware(req) {
+    // Caso queira adicionar lógicas extras, como verificar role do usuário:
+    const token = req.nextauth.token;
+    if (req.nextUrl.pathname.startsWith("/admin") && token?.role !== "ADMIN") {
+      return NextResponse.rewrite(new URL("/login", req.url));
+    }
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+    pages: {
+      signIn: "/login",
+    },
+  }
+);
+
+export const config = {
+  matcher: ["/admin/:path*"],
+};
